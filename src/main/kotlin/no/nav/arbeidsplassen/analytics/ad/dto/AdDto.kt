7008fe7446979922e8f22573b4ebbafa10930b6c
Yong Bin Kwon <yongbinkwon97@gmail.com>
2020-07-21 14:41:32 +0200

    start using dimensionentity

--- src/main/kotlin/no/nav/arbeidsplassen/analytics/ad/dto/AdDto.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/analytics/ad/dto/AdDto.kt
@@ -1,14 +1,41 @@
 package no.nav.arbeidsplassen.analytics.ad.dto
 
+import com.google.api.services.analyticsreporting.v4.model.ReportRow
+
 data class AdDto(
     var sidevisninger: Int = 0,
     //leaving the actual calculation to frontend
     var average: List<Double> = listOf(),
     var referrals: List<String> = listOf(),
-    var viewsPerReferral: List<Int> = listOf()
+    var viewsPerReferral: List<Int> = listOf(),
+    var viewsOverTime: Map<String, String>
     /*
-    var viewsOverTime: Map<String, String>,
     var region: Map<String, String>,
     var device: Map<String, String>
      */
 )
+
+//kanskje ha annet sted
+abstract class DimensionEntity {
+    val dimensions: List<String>
+    val metrics: List<String>
+
+    constructor(row: ReportRow) {
+        dimensions = row.dimensions
+        metrics = row.getMetric()
+    }
+
+    abstract fun toAdDto()
+}
+
+private fun ReportRow.getMetric() = metrics.first().getValues()
+
+class ReferralEntity(row: ReportRow): DimensionEntity(row) {
+
+    //ikke sikker på return eller dto-variabel
+    override fun toAdDto() {
+        val(
+
+        )
+    }
+}
